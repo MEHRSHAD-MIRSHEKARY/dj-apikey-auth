32a5844154766715fac9f11b5e20521fa062c812
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2025-03-20 11:55:04 +0330

    :sparkles: feat(api): Add AdminAPIKeyViewSet for admins & retrict user access
    Closes #53

--- apikey_auth/api/serializers/api_key.py
+++ apikey_auth/api/serializers/api_key.py
@@ -1,12 +1,26 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from apikey_auth.models import APIKey
+from django.utils.translation import gettext_lazy as _
+
+
+User = get_user_model()
 
 
 class APIKeySerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField(
+        allow_null=True,
+        required=False,
+        write_only=True,
+        label=_("User ID"),
+        help_text=_("The ID of the user associated with this APIKey (Optional)"),
+    )
+
     class Meta:
         model = APIKey
         fields = [
             "id",
+            "user_id",
             "user",
             "key",
             "created_at",
@@ -27,6 +41,14 @@ class APIKeySerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        """Ensure API key is generated on creation."""
-        validated_data["user"] = self.context["request"].user
+        """Ensure API key is generated on creation with the validated user."""
+        user_id = validated_data.pop("user_id", None)  # Remove user_id from validated_data
+        if user_id:
+            try:
+                user = User.objects.get(id=user_id)
+            except User.DoesNotExist:
+                raise serializers.ValidationError(
+                    {"user_id": _("User with the provided user_id does not exist.")}
+                )
+            validated_data["user"] = user
         return super().create(validated_data)
