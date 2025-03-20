5eef7d51b1fd0833cb3248236e996cd857591738
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2025-03-20 11:57:31 +0330

    :rotating_light: test(API): Add unit test for user APIs & modify Admin related APIs
    Closes #54

--- apikey_auth/urls.py
+++ apikey_auth/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from apikey_auth.api.views import APIKeyViewSet
+from apikey_auth.api.views import APIKeyViewSet, AdminAPIKeyViewSet
 from apikey_auth.views import APIKeyListView
 
 router = DefaultRouter()
-router.register(r"apikey", APIKeyViewSet, basename="api-key")
+router.register("apikey", AdminAPIKeyViewSet, basename="api-key")
+router.register("my-apikey", APIKeyViewSet, basename="my-api-key")
 
 urlpatterns = [
     path("api_keys/", APIKeyListView.as_view(), name="api_keys"),
